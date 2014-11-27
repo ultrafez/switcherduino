b73d868c42abf62652df03a3420afae32db32ccd
Alex Silcock <alex@alexsilcock.net>
2014-11-27 02:03:09 +0000

    Handle multiple commands in sequence

--- arduino.ino
+++ arduino.ino
@@ -38,8 +38,11 @@ void setup() {
 
   // Radio pin setup
   digitalWrite(GND_PIN, LOW);
+
+  setupFSM();
   
   Serial.begin(115200);
+  Serial.println("Maplin remote socket controller. Send commands in the regex format rs([1-4][1-4][01])*\\n to turn on/off sockets in channel 1-4 and button 1-4");
 }
 
 void sendData(long payload1, long payload2) {
@@ -112,19 +115,28 @@ void loop() {
 // States
 const int STATE_WAIT_FOR_R = 1;
 const int STATE_WAIT_FOR_S = 2;
-const int STATE_WAIT_FOR_CHANNEL = 3;
+const int STATE_READY_FOR_CHANNEL_OR_EXECUTE = 3;
 const int STATE_WAIT_FOR_BUTTON = 4;
 const int STATE_WAIT_FOR_ONOFF = 5;
-const int STATE_WAIT_FOR_EXECUTE = 6;
 
 // Machine stored data
 int currentState = STATE_WAIT_FOR_R;
 int chosenChannel = 0;
 int chosenButton = 0;
-int chosenOnOff = -1;
+
+// store a list of commands to execute when the execute command is received.
+// each command is represented by 3 consecutive array values - channel, button, state.
+// set to -1 when they don't contain a command to execute
+char commandsToExec[16][3];
+int nextCommandIndex = 0; // what index should we insert the next command into?
+
+void setupFSM() {
+  clearCommandsToExec();
+}
 
 /**
- * Simulate a FSM that parses strings of the format "rs131\r", meaning to set channel 1, button 3, to state 1 (on).
+ * Simulate a FSM that parses strings of the format "rs131*\r", meaning to set channel 1, button 3, to state 1 (on).
+ * Handles multiple commands (up to a total of 16), e.g. rs131140\r to turn on channel 1 btn 3, and turn off channel 1 btn 4
  * \n may be used instead of \r
  */
 void updateFSM(char inputChar) {
@@ -132,29 +144,39 @@ void updateFSM(char inputChar) {
   switch (currentState) {
     case STATE_WAIT_FOR_R:
       if (inputChar == 'r') {
-        Serial.println("Change state to wait for s");
+        Serial.println("-> STATE_WAIT_FOR_S");
         currentState = STATE_WAIT_FOR_S;
       }
       break;
 
     case STATE_WAIT_FOR_S:
       if (inputChar == 's') {
-        Serial.println("Change state to wait for channel");
-        currentState = STATE_WAIT_FOR_CHANNEL;
+        Serial.println("-> STATE_READY_FOR_CHANNEL_OR_EXECUTE");
+        currentState = STATE_READY_FOR_CHANNEL_OR_EXECUTE;
       } else {
-        Serial.println("Change state to wait for r");
+        clearCommandsToExec();
+        Serial.println("-> STATE_WAIT_FOR_R");
         currentState = STATE_WAIT_FOR_R;
       }
       break;
 
-    case STATE_WAIT_FOR_CHANNEL:
+    case STATE_READY_FOR_CHANNEL_OR_EXECUTE:
+      if (inputChar == 13 || inputChar == 10) { // carriage return or line feed
+        executeCommands();
+        clearCommandsToExec();
+        Serial.println("-> STATE_WAIT_FOR_R");
+        currentState = STATE_WAIT_FOR_R;
+        break;
+      }
+
       charAsInt = inputChar - '0';
       if (charAsInt >= 1 && charAsInt <= 4) {
         chosenChannel = charAsInt;
-        Serial.println("Change state to wait for button");
+        Serial.println("-> STATE_WAIT_FOR_BUTTON");
         currentState = STATE_WAIT_FOR_BUTTON;
       } else {
-        Serial.println("Change state to wait for r");
+        clearCommandsToExec();
+        Serial.println("-> STATE_WAIT_FOR_R");
         currentState = STATE_WAIT_FOR_R;
       }
       break;
@@ -163,10 +185,11 @@ void updateFSM(char inputChar) {
       charAsInt = inputChar - '0';
       if (charAsInt >= 1 && charAsInt <= 4) {
         chosenButton = charAsInt;
-        Serial.println("Change state to wait for onoff");
+        Serial.println("-> STATE_WAIT_FOR_ONOFF");
         currentState = STATE_WAIT_FOR_ONOFF;
       } else {
-        Serial.println("Change state to wait for r");
+        clearCommandsToExec();
+        Serial.println("-> STATE_WAIT_FOR_R");
         currentState = STATE_WAIT_FOR_R;
       }
       break;
@@ -174,22 +197,46 @@ void updateFSM(char inputChar) {
     case STATE_WAIT_FOR_ONOFF:
       charAsInt = inputChar - '0';
       if (charAsInt == 0 || charAsInt == 1) {
-        chosenOnOff = charAsInt;
-        Serial.println("Change state to wait for execute");
-        currentState = STATE_WAIT_FOR_EXECUTE;
+        addCommand(chosenChannel, chosenButton, charAsInt);
+        Serial.println("-> STATE_READY_FOR_CHANNEL_OR_EXECUTE");
+        currentState = STATE_READY_FOR_CHANNEL_OR_EXECUTE;
       } else {
-        Serial.println("Change state to wait for r");
+        clearCommandsToExec();
+        Serial.println("-> STATE_WAIT_FOR_R");
         currentState = STATE_WAIT_FOR_R;
       }
       break;
+  }
+}
 
-    case STATE_WAIT_FOR_EXECUTE:
-      if (inputChar == 13 || inputChar == 10) { // carriage return or line feed
-        Serial.println("Simulating button");
-        simulateButton(chosenChannel, chosenButton, chosenOnOff);
-      }
-      Serial.println("Change state to wait for r");
-      currentState = STATE_WAIT_FOR_R;
-      break;
+void addCommand(int channel, int button, int onOff) {
+  Serial.print("Adding command to queue: channel ");
+  Serial.print((int)channel);
+  Serial.print(", button ");
+  Serial.print((int)button);
+  Serial.print(", power ");
+  Serial.println((int)onOff);
+  commandsToExec[nextCommandIndex][0] = channel;
+  commandsToExec[nextCommandIndex][1] = button;
+  commandsToExec[nextCommandIndex][2] = onOff;
+  if (nextCommandIndex < 15) {
+    nextCommandIndex += 1;
+  }
+}
+
+void clearCommandsToExec() {
+  Serial.println("Clearing button command queue");
+  for (int i=0; i<16; i++) {
+    commandsToExec[i][0] = -1; // only set the channel to -1, because only the channel is checked for emptiness
+  }
+  nextCommandIndex = 0;
+}
+
+void executeCommands() {
+  Serial.println("Executing button command queue");
+  for (int i=0; i<16; i++) {
+    if (commandsToExec[i][0] != -1) {
+      simulateButton(commandsToExec[i][0], commandsToExec[i][1], commandsToExec[i][2]);
+    }
   }
 }
